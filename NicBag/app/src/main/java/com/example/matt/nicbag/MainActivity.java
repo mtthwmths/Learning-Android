d5b6c7e34345589c7924730f61aa96a76a02fa61
Matt M <mtthwmths@gmail.com>
2018-08-05 19:03:43 -0500

    NicBag is just a buncha buttons
    with no where to go.

---
+++ NicBag/app/src/main/java/com/example/matt/nicbag/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.matt.nicbag;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.matt.MESSAGE";
+    public static final String TOTAL_COUNT = "total_count";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    /**
+     * Show a toast
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view){
+        //Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Here's to health and needing friends!", Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+    /**
+     * increment the count number by one when the count button is clicked
+     * @param view
+     */
+    public void countMe(View view){
+        //get the middle number
+        TextView showCountTextView = (TextView) findViewById(R.id.numberBox);
+
+        //get the value of the text view, convert it to a number and increment it.
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+
+        //update the text view with the new number
+        showCountTextView.setText(count.toString());
+    }
+
+    public void randomMe(View view){
+        //create the intent to start the second activity
+        Intent randomIntent = new Intent(this, RandomCount.class);
+        TextView countTextView = (TextView) findViewById(R.id.numberBox);
+        String countString = countTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        randomIntent.putExtra(TOTAL_COUNT, count);
+        startActivity(randomIntent);
+    }
+}
---
+++ NicBag/app/src/main/java/com/example/matt/nicbag/RandomCount.java
@@ -0,0 +1,39 @@
+package com.example.matt.nicbag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class RandomCount extends AppCompatActivity {
+
+    private static final String TOTAL_COUNT = "total_count";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_count);
+        showRandomNumber();
+    }
+
+    public void showRandomNumber () {
+        TextView randomView = (TextView) findViewById(R.id.random_number_display);
+        TextView headingView = (TextView) findViewById(R.id.random_number_label);
+
+        int count = getIntent().getIntExtra(TOTAL_COUNT, 0);
+
+        //generate the random number and display it in the correct text view.
+        Random randomNumber = new Random();
+        int randomInt = 0;
+        if (count > 0){
+            randomInt = randomNumber.nextInt(count);
+        }
+
+        randomView.setText(Integer.toString(randomInt));
+
+        //substitute the %1d in the heading with count.
+        headingView.setText(getString(R.string.random_number_info, count));
+    }
+
+}
